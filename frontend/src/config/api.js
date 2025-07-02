8cfb4694238a947ed0a6a920f088166571aac3af
Dilmurod <101713345+Dilmurodmirzakulov@users.noreply.github.com>
2025-07-02 11:08:43 +0500

    update

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL 
-  // 'https://techamal-production.up.railway.app/api';
+  import.meta.env.VITE_API_URL ||
+  'https://techamal-production.up.railway.app/api';
 
 export default API_BASE_URL;
