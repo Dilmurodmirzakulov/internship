ff42b0037445109acfb6d5d6394d5ee2ec7e1e88
Dilmurod <101713345+Dilmurodmirzakulov@users.noreply.github.com>
2025-08-11 17:40:10 +0500

    https and cors url

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://127.0.0.1:3000",
   "https://beautiful-boba-4352fc.netlify.app",
-  "http://techamal.uz",
+  "https://techamal.uz",
   "https://685f9488966d03230038ae17--beautiful-boba-4352fc.netlify.app", // Latest deploy URL
   "https://techamal-production.up.railway.app", // Add your Railway public URL
   "https://api.techamal.uz/",
